68db1126b96b2c69255471a90514fbba5f1fc1e0
coderdba <coderdba@coder.com>
2019-12-15 11:16:58 +0530

    updates

---
+++ kubernetes/kube-vbox-ans-singlenode-15.5/deploy-full.sh
@@ -0,0 +1,2 @@
+#ansible-playbook -u root --private-key=~/.ssh/myansible.priv -i inv/ksn2.inv deploy-full.yml
+ansible-playbook -u root --private-key=~/.ssh/myansible.priv -i inv/ksn3.inv deploy-full-ksn3.yml
